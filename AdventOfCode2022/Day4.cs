c2e6b1641cc4f20366b0ecdf676d7a49324c0574
Thomas Godon <thomas.godon@gmail.com>
2022-12-04 21:02:36 +0100

    fix day4

---
+++ AdventOfCode2022/Day4.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022
+{
+    internal class Day4 : DayBase
+    {
+        public Day4() : base(4)
+        {
+            var doublePairs = 0;
+            foreach (var item in Properties.Resources.InputDay4.Split("\r\n"))
+            {
+                var startPair1 = int.Parse(item.Split(',')[0].Split('-')[0]);
+                var endPair1 = int.Parse(item.Split(',')[0].Split('-')[1]);
+                var startPair2 = int.Parse(item.Split(',')[1].Split('-')[0]);
+                var endPair2 = int.Parse(item.Split(',')[1].Split('-')[1]);
+
+                var sequencePair1 = Enumerable.Range(startPair1, endPair1 - startPair1 + 1).ToList();
+                var sequencePair2 = Enumerable.Range(startPair2, endPair2 - startPair2 + 1).ToList();
+
+                if (sequencePair1.Intersect(sequencePair2).Count() == sequencePair1.Count ||
+                    sequencePair2.Intersect(sequencePair1).Count() == sequencePair2.Count)
+                {
+                    doublePairs++;
+                }
+            }
+
+            Console.WriteLine($"Total doubles: {doublePairs}");
+            doublePairs = 0;
+
+            foreach (var item in Properties.Resources.InputDay4.Split("\r\n"))
+            {
+                var startPair1 = int.Parse(item.Split(',')[0].Split('-')[0]);
+                var endPair1 = int.Parse(item.Split(',')[0].Split('-')[1]);
+                var startPair2 = int.Parse(item.Split(',')[1].Split('-')[0]);
+                var endPair2 = int.Parse(item.Split(',')[1].Split('-')[1]);
+
+                var sequencePair1 = Enumerable.Range(startPair1, endPair1 - startPair1 + 1).ToList();
+                var sequencePair2 = Enumerable.Range(startPair2, endPair2 - startPair2 + 1).ToList();
+
+                if (sequencePair1.Intersect(sequencePair2).Count() > 0)
+                {
+                    doublePairs++;
+                }
+            }
+
+            Console.WriteLine($"Total doubles: {doublePairs}");
+        }
+    }
+}
--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -3,3 +3,4 @@
 var day1 = new Day1();
 var day2 = new Day2();
 var day3 = new Day3();
+var day4 = new Day4();
